32bfcb3d9f2c71a3f03547ace6c45062e842c135
FruitSalad1405 <stevensentosa8@gmail.com>
2020-04-16 18:42:46 +0700

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Bitch Ass';
+  minum = [{
+    nama : 'Coca Cola',
+    harga : 6500
+  }, {
+    nama : 'Sprite',
+    harga:6000
+  }]
+  
+  itemArr = [1,2,3,4]
+
+  show : boolean = true;
+
+  user = {
+    name: 'Fruit Salad'
+  };
 }
